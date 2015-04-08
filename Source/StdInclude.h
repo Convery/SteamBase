72ef4c102b914a5cabd9cbe13acd287e63e84930
Convery <convery10122@gmail.com>
2015-04-08 16:03:32 +0200

    Interface manager added.

--- Source/StdInclude.h
+++ Source/StdInclude.h
@@ -104,11 +104,10 @@
 
 // Steam headers.
 #pragma region SteamIncludes
-//#include "Steam\Interface.h"
+#include "Steam\InterfaceManager.h"
 //#include "Steam\Callback.h"
 
 #include "Steam\Common\ClientCommon.h"
 #include "Steam\SteamTypes\SteamTypes.h"
 #include "Steam\Interface\SteamClient.h"
-
 #pragma endregion
