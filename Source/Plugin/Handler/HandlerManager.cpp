7f8349d40bcf1523ce83dcbca2740d5e340ea0a9
Maurice Heumann <MauriceHeumann@gmail.com>
2015-05-26 18:06:15 +0200

    Some fixes.

--- Source/Plugin/Handler/HandlerManager.cpp
+++ Source/Plugin/Handler/HandlerManager.cpp
@@ -2,30 +2,50 @@
 
 static CRITICAL_SECTION ThreadSafe;
 
-bool HandlerManager::RegisterHandler(Handler_Event EventType, PluginBase* HandlerBase){
+std::unordered_map<Handler_Event, PluginBase *> HandlerManager::Instances;
+
+bool HandlerManager::RegisterHandler(Handler_Event EventType, PluginBase* HandlerBase)
+{
 	EnterCriticalSection(&ThreadSafe);
-	switch (EventType){
+
+	switch (EventType)
+	{
 	case Handler_Event::k_HNetworking:
 		//this is just not safe
-		if(HandlerBase->Name.compare("HedgeNode")){
+		if(HandlerBase->Name.compare("HedgeNode"))
+		{
 			Instances[EventType] = HandlerBase;
-		}else{
+		}
+		else
+		{
 			DBGPrint("Plugin %s tried to register as network handler", HandlerBase->Name);
 		}
 		break;
+
 	case Handler_Event::k_HGenericEvent:
 		Instances[EventType] = HandlerBase;
-	default:break;
+
+	default:
+		break;
 	}
+
 	Instances[EventType] = HandlerBase;
+
 	LeaveCriticalSection(&ThreadSafe);
+
+	return true;
 }
 
-bool HandlerManager::SendEvent(Handler_Event EventType, const char *Message, void * Callback){
+bool HandlerManager::SendEvent(Handler_Event EventType, const char *Message, void * Callback)
+{
 	EnterCriticalSection(&ThreadSafe);
+
 	if (Instances.find(EventType) != Instances.end())
 	{
 		Instances[EventType]->SendMessageB(Message, Callback);
 	}
+
 	LeaveCriticalSection(&ThreadSafe);
+
+	return true; // ?
 }
